9a6d8bfab2c9083d1852b57fe84c2c8c34ea7f9b
Petras <petrasvilkelis@gmail.com>
2021-06-01 11:01:18 +0200

    Initial commit

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as Chakra from '@chakra-ui/react';
+
+const NotFoundPage = (props) => {
+
+    return (
+        <Chakra.Box>
+            404 Page
+        </Chakra.Box>
+    );
+}
+
+export default NotFoundPage;
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as Chakra from '@chakra-ui/react';
+
+const Index = (props) => {
+
+    return (
+        <Chakra.Box>
+            Index Page
+        </Chakra.Box>
+    );
+}
+
+export default Index;
